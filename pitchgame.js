43cf8876909dc0f83881d07ba5a680be6863c8f7
Paul Kienitz <paul@paulkienitz.net>
2021-10-07 14:42:42 -0700

    0.2: no longer inline in pitchgame.php

---
+++ pitchgame.js
@@ -0,0 +1,102 @@
+function polyfill_closest()
+{			// this is taken from MDN:
+	if (!Element.prototype.matches)
+		Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
+	if (!Element.prototype.closest)
+		Element.prototype.closest = function(s) {
+			var el = this;
+			do {
+				if (Element.prototype.matches.call(el, s))
+					return el;
+				el = el.parentElement || el.parentNode;
+			} while (el !== null && el.nodeType === 1);
+			return null;
+		};
+}
+
+function showHints(ev)
+{
+	var plopup = document.getElementById('howtoplay');
+	plopup.style.display = 'block';
+	ev.preventDefault();
+	ev.stopPropagation();
+	return false;
+}
+
+function closer()
+{
+	this.closest('.plop').style.display = 'none';
+}
+
+function beModerate(ev)
+{
+	var formtype = document.getElementById('formtype');
+	var form = document.getElementById('pitchform');
+	if (form && formtype)
+	{
+		formtype.value = 'moderate';
+		form.submit();
+	}
+	ev.preventDefault();
+	ev.stopPropagation();
+	return false;
+}
+
+function showStars(pitchId, rating)
+{
+	for (var r = 1; r <= 4; r++)
+	{
+		var starId = "star_" + r + "_" + pitchId;
+		var star = document.getElementById(starId);
+		star.innerHTML = r <= rating ? "&#9733;" : "&#9734;";    // solid star vs hollow star
+	}
+	var spamId = "spam_" + pitchId;
+	var spam = document.getElementById(spamId);
+	spam.innerHTML = rating < 0 ? "Marked as spam" : "Mark as spam";
+	if (rating < 0)
+		spam.classList.add("marked");
+	else
+		spam.classList.remove("marked");
+	var field = document.getElementById("newrating_" + pitchId);
+	field.value = rating == 0 ? "" : rating + "";
+}
+
+function rateWithStars(ev)
+{
+	var parts = /star_(\d+)_(\d+)/.exec(this.id);
+	var stars = parseInt(parts[1]);
+	var pitchId = parseInt(parts[2]);
+	showStars(pitchId, stars);
+}
+
+function rateAsSpamOrClear(ev)
+{
+	var parts = /([a-z]+)_(\d+)/.exec(this.id);
+	var pitchId = parseInt(parts[2]);
+	showStars(pitchId, parts[1] == "spam" ? -1 : 0);
+}
+
+function init()
+{
+	polyfill_closest();
+	var showhints = document.getElementById('showhints');
+	if (showhints)
+		showhints.addEventListener('click', showHints);
+	var closers = document.getElementsByClassName('closer');
+	for (var i = 0; i < closers.length; i++)
+		closers[i].addEventListener('click', closer);
+	var backers = document.getElementsByClassName('backer');
+	for (var i = 0; i < backers.length; i++)
+		backers[i].addEventListener('click', closer);
+	var moderato = document.getElementById('moderato');
+	if (moderato)
+		moderato.addEventListener('click', beModerate);
+	var stars = document.getElementsByClassName('star');
+	for (var i = 0; i < stars.length; i++)
+		stars[i].addEventListener('click', rateWithStars);
+	var stars = document.getElementsByClassName('spam');
+	for (var i = 0; i < stars.length; i++)
+		stars[i].addEventListener('click', rateAsSpamOrClear);
+}
+
+window.addEventListener("DOMContentLoaded", init);
